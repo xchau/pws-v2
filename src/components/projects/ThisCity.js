0c9519c54435d151b722ce8da2362f69e7d79982
Minh Chau <xiu.chau@gmail.com>
2017-08-26 17:11:24 -0700

    deploy to heroku

--- src/components/projects/ThisCity.js
+++ src/components/projects/ThisCity.js
@@ -35,24 +35,44 @@ export const ThisCity = (props) => {
       <div className="project-divider"></div>
       <div className="project-description-container">
         <div className="project-description-box">
-          <p>Remember that giant pothole just around the corner that you always have to swerve to avoid? Everyone gripes about it, but the city doesn't seem to know. With this.city, community members can pin areas of caution or interest to a shared map and draw attention to the issues that affect our daily lives.</p>
+          <p>The idea for this.city came to my
+            <a
+              alt="Link to Jamieson's GitHub"
+              href="https://github.com/jamiesonbates"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              project partner
+            </a>
+            and I during one of mini-breaks from brainstorming. Jamieson started to gripe about a noisy dog kennel around the corner from his apartment. We soon realized that there were very little measures a person could take to address some of the issues around their neighborhood. There was a Waze for traffic, but not for noisy dog kennels, broken glass, potholes or potential danger.
+          </p>
           <div className="project-img-row">
             <div className="project-img-box">
               <img
-                alt="Blip pic 2"
-                className="project-img-one"
-                src="https://res.cloudinary.com/xchau/image/upload/v1491348232/kkahrfhlaorxxi2jg3my.jpg" />
+                alt="this.city login"
+                className="project-img-two-small"
+                src="https://res.cloudinary.com/xchau/image/upload/v1503705591/login_fmklgn.png" />
+              <img
+                alt="this.city map view"
+                className="project-img-two-small"
+                src="https://res.cloudinary.com/xchau/image/upload/v1503705591/map_dzxrsd.png" />
             </div>
           </div>
         </div>
         <div className="project-description-box">
-          <p>Remember that giant pothole just around the corner that you always have to swerve to avoid? Everyone gripes about it, but the city doesn't seem to know. With this.city, community members can pin areas of caution or interest to a shared map and draw attention to the issues that affect our daily lives.</p>
+          <p>
+            The gist of this.city is fairly simple. Users can pin their gripes (from a variety of categories) to a map. This map is visible to all this.city users. Fellow users can then verify the claim or not. The end goal is to promote attention and awareness, as well as to warn others of potential danger in their area.
+          </p>
           <div className="project-img-row">
             <div className="project-img-box">
               <img
-                alt="Blip pic 2"
-                className="project-img-one"
-                src="https://res.cloudinary.com/xchau/image/upload/v1491348232/kkahrfhlaorxxi2jg3my.jpg" />
+                alt="this.city add issue"
+                className="project-img-two-small"
+                src="https://res.cloudinary.com/xchau/image/upload/v1503705591/report_jheu2y.png" />
+              <img
+                alt="this.city verify"
+                className="project-img-two-small"
+                src="https://res.cloudinary.com/xchau/image/upload/v1503705591/problem_stxrqn.png" />
             </div>
           </div>
         </div>
