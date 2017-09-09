8c303c1710e84712c2f5c3c1ad4218296942e411
Minh Chau <xiu.chau@gmail.com>
2017-09-08 17:08:06 -0700

    address skewed about hero

--- src/components/About.js
+++ src/components/About.js
@@ -7,19 +7,23 @@ import resume from '../assets/files/chau-minh-resume.pdf';
 export const About = (props) => {
   return (
     <div className="content-box">
-      <div className="about-hero">
-        <div className="about-profile-pic"></div>
+      <div className="about-container">
+        <div className="about-hero">
+          <div className="about-profile-pic"></div>
 
-        Hello world! Pleased to meet you. I'm a web dev with experience teaching and working with diverse groups. My goal is to promote diversity, tolerance, and social welfare through app development (especially relevant in this day and age). Let's get in touch. First drink is on me!
+          <span>
+            Hello world! Pleased to meet you. I'm a web dev with experience teaching and working with diverse groups. My goal is to promote diversity, tolerance, and social welfare through app development (especially relevant in this day and age). Let's get in touch. First drink is on me!
+          </span>
 
-        <a
-          download="chau-minh-resume.pdf"
-          href={resume}
-        >
-          <div className="about-dl-resume">
-            Résumé
-          </div>
-        </a>
+          <a
+            download="chau-minh-resume.pdf"
+            href={resume}
+          >
+            <div className="about-dl-resume">
+              Résumé
+            </div>
+          </a>
+        </div>
       </div>
     </div>
   );
